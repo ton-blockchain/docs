0a4ccc4ff44db7fe073df30f10df0ed6d38c4bec
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-06-10 21:23:57 +0200

    Remove the color splash on the index page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -141,20 +141,6 @@ export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
       <div className="relative isolate mx-auto w-full max-w-5xl px-6 py-12 text-left sm:py-16">
-        {/* color splash */}
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-60"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#2d83ec] to-[#1ac9ff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
-          />
-        </div>
-
         {/* hero */}
         <section className="flex flex-col items-start gap-8 py-8 lg:flex-row lg:items-center lg:gap-12 lg:py-12">
           <img
