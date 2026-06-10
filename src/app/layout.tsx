c5e2f2244c41d23f38462ec4cc7fbee2d8236adb
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-06-10 23:36:46 +0200

    Remove the warning banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Provider } from '@/components/provider';
-import { Banner } from '@/components/mdx/banner';
 import logoDark from "@/public/logo/dark.svg"
 import logoLight from "@/public/logo/light.svg"
 
@@ -33,20 +32,6 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Banner
-          variant="rainbow"
-          rainbowColors={[
-            'rgba(255,100,0, 0.5)',
-            'transparent',
-            'rgba(255,100,0, 0.5)',
-            'transparent',
-            'rgba(255,100,0, 0.5)',
-            'transparent',
-          ]}
-          changeLayout={false}
-        >
-          <b>WARNING:</b>&nbsp;This is an experimental setup that is currently in the works. Not ready for public use!
-        </Banner>
         <Provider>
           {children}
         </Provider>
