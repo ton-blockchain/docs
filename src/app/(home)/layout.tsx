a3413509d902ea72cbeeb843537359f86934cba3
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-06-21 03:15:59 +0200

    More logos on the home page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { gitConfig } from '@/lib/shared';
-import { ThemeLogo } from '@/components/ui/logo';
+import { ThemeLogo, Telegram, XTwitter } from '@/components/ui/logo';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -11,6 +11,22 @@ export default function Layout({ children }: { children: ReactNode }) {
         title: <ThemeLogo />,
       }}
       githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}`}
+      links={[
+        {
+          type: 'icon',
+          icon: <XTwitter />,
+          text: 'X/Twitter',
+          url: 'https://x.com/ton_blockchain',
+          external: true,
+        },
+        {
+          type: 'icon',
+          icon: <Telegram />,
+          text: 'Telegram',
+          url: 'https://t.me/addlist/1r5Vcb8eljk5Yzcy',
+          external: true,
+        },
+      ]}
     >
       {children}
     </HomeLayout>
