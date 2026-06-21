beee093ab970f92566513693b90aaae1810c537d
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-06-21 06:42:37 +0200

    ? disable quick jumps

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useMemo } from 'react';
-import { useRouter } from 'next/navigation';
-import { ArrowRight } from 'lucide-react';
+// import { useMemo } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { ArrowRight } from 'lucide-react';
 import {
   SearchDialog,
   SearchDialogClose,
@@ -14,14 +14,14 @@ import {
   SearchDialogList,
   SearchDialogOverlay,
   type SharedProps,
-  type SearchItemType,
+  // type SearchItemType,
   // TagsList,
   // TagsListItem,
 } from 'fumadocs-ui/components/dialog/search';
 import { useDocsSearch } from 'fumadocs-core/search/client';
-// import { flexsearchStaticClient } from 'fumadocs-core/search/client/flexsearch-static';
+import { flexsearchStaticClient } from 'fumadocs-core/search/client/flexsearch-static';
 import { useI18n } from 'fumadocs-ui/contexts/i18n';
-import { getQuickJumpPages } from '@/lib/source';
+// import { getQuickJumpPages } from '@/lib/source';
 
 export default function DefaultSearchDialog(props: SharedProps) {
   // const [tag] = useState<string | undefined>();
@@ -34,37 +34,38 @@ export default function DefaultSearchDialog(props: SharedProps) {
         }
       : {
           type: 'flexsearch-static',
-          // client: flexsearchStaticClient(),
+          client: flexsearchStaticClient(),
           locale,
           from: `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/api/search`,
           // tag,
         },
   );
-  const router = useRouter();
-  const pages = getQuickJumpPages();
-  const quickJumpAction = useMemo<SearchItemType | undefined>(() => {
-    if (search.length === 0) return;
+  // const router = useRouter();
+  // const pages = getQuickJumpPages();
+  // const quickJumpAction = useMemo<SearchItemType | undefined>(() => {
+  //   if (search.length === 0) return;
 
-    const normalized = search.toLowerCase();
-    for (const page of pages) {
-      // NOTE: this could be a fuzzy search instead
-      if (!page.title.toLowerCase().startsWith(normalized)) continue;
+  //   const normalized = search.toLowerCase();
+  //   for (const page of pages) {
+  //     // NOTE: this could be a fuzzy search instead
+  //     if (!page.title.toLowerCase().startsWith(normalized)) continue;
 
-      return {
-        id: 'quick-action',
-        type: 'action',
-        node: (
-          <div className="inline-flex items-center gap-2 text-fd-muted-foreground">
-            <ArrowRight className="size-4" />
-            <p>
-              Jump to <span className="font-medium text-fd-foreground">{page.title}</span>
-            </p>
-          </div>
-        ),
-        onSelect: () => router.push(page.url),
-      };
-    }
-  }, [router, search]);
+  //     return {
+  //       id: 'quick-action',
+  //       type: 'action',
+  //       node: (
+  //         <div className="inline-flex items-center gap-2 text-fd-muted-foreground">
+  //           <ArrowRight className="size-4" />
+  //           <p>
+  //             Jump to <span className="font-medium text-fd-foreground">{page.title}</span>
+  //           </p>
+  //         </div>
+  //       ),
+  //       onSelect: () => router.push(page.url),
+  //     };
+  //   }
+  // }, [router, search]);
+  const quickJumpAction = undefined;
 
   return (
     <SearchDialog search={search} onSearchChange={setSearch} isLoading={query.isLoading} {...props}>
