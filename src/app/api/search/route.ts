14c77c87d057e1011f26263ce8354bc496a11727
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-08-20 19:56:10 +0200

    try pt15 from flexsearch

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,12 +1,15 @@
 import { source, getSearchablePages } from '@/lib/source';
 // See: https://www.zbsearch.dev/
 import { createFromSource } from 'fumadocs-core/search/server';
+import { pluginPT15 } from '@zbsearch/plugin-pt15';
 
 const searchSource: typeof source = {
   ...source,
   getPages: getSearchablePages,
 };
-const searchAPI = createFromSource(searchSource);
+const searchAPI = createFromSource(searchSource, {
+  plugins: [pluginPT15()]
+});
 
 export const revalidate = false;
 export const GET = process.env.NEXT_CONFIG === 'vercel' ? searchAPI.GET : searchAPI.staticGET;
