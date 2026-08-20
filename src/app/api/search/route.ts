60bc52ffddba7409c5cf7e1c87b66531a05838b9
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-08-20 18:26:49 +0200

    try new zbsearch (orama fork) and remove unnecessary i18n

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,5 +1,6 @@
 import { source, getSearchablePages } from '@/lib/source';
-import { flexsearchFromSource } from 'fumadocs-core/search/flexsearch';
+import { createFromSource } from 'fumadocs-core/search/server';
+// import { flexsearchFromSource } from 'fumadocs-core/search/flexsearch';
 
 const searchSource: typeof source = {
   ...source,
@@ -7,23 +8,24 @@ const searchSource: typeof source = {
 };
 
 // https://www.fumadocs.dev/docs/headless/search/flexsearch#static-export
-const searchAPI = flexsearchFromSource(searchSource, {
-  document: {
-    encoder: 'LatinExtra',
-    tokenize: 'full',
-  },
-  // async buildIndex(page) {
-  //   return {
-  //     title: page.data.title,
-  //     description: page.data.description,
-  //     url: page.url,
-  //     id: page.url,
-  //     structuredData: await page.data.structuredData(),
-  //     breadcrumbs: page.slugs.slice(0, -1),
-  //     // tag: undefined,
-  //   };
-  // }
-});
+// const searchAPI = flexsearchFromSource(searchSource, {
+//   document: {
+//     encoder: 'LatinExtra',
+//     tokenize: 'full',
+//   },
+//   // async buildIndex(page) {
+//   //   return {
+//   //     title: page.data.title,
+//   //     description: page.data.description,
+//   //     url: page.url,
+//   //     id: page.url,
+//   //     structuredData: await page.data.structuredData(),
+//   //     breadcrumbs: page.slugs.slice(0, -1),
+//   //     // tag: undefined,
+//   //   };
+//   // }
+// });
+const searchAPI = createFromSource(searchSource);
 
 export const revalidate = false;
 export const GET = process.env.NEXT_CONFIG === 'vercel' ? searchAPI.GET : searchAPI.staticGET;
