4650754ddca5d793cb572dd35366ff6fce02ee28
Novus Nota <68142933+novusnota@users.noreply.github.com>
2026-09-14 15:22:12 +0200

    chore(debug): build with writing NEXT_BUILD_TYPE to /.well-known/build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+import { writeFileSync } from 'node:fs';
 import staticConfig from './next.config.static';
 import vercelConfig from './next.config.vercel';
 
 const requestedConfig = process.env.NEXT_CONFIG === 'vercel' ? vercelConfig : staticConfig;
 console.log(requestedConfig.env);
+writeFileSync('./public/.well-known/build', requestedConfig.env?.NEXT_BUILD_TYPE ?? 'unknown');
 export default requestedConfig;
